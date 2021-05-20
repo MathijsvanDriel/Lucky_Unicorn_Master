61a6cbd12ca08b58e0a7eb92a405b26f97db167d
thijsvan10 <>
2021-05-20 22:40:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,1 @@
+from token_v3 import *
---
+++ token_v3.py
@@ -0,0 +1,7 @@
+#set balance for testing purposes 
+balance = 5
+
+rounds_played = 0 
+
+play_again = input("Press ,<Enter> to play...")
+while play_again == "": 
